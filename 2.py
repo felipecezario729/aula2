301e471098cded044b3373a5f6964eaa097fb6df
Felipe Cezario <166965964+felipecezario729@users.noreply.github.com>
2025-07-11 20:41:41 -0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,9 @@
+# posso fazer um comentario
+print('oi') # irá imprimir oi na tela (quando digitar cls apagar no terminal)
+# docString shift mais aspas 
+"""
+agora tudo que eu 
+digitar, dentro 
+destas aspas, será 
+ignorado pelo programa,
+"""
---
+++ 3.py
@@ -0,0 +1,7 @@
+print (10, 20,30, sep=" - ", end="#")
+print (50, 60,70, sep=" * ", end="\n")
+print (80, 90,100, sep=" * ", end="\n")
+print('douglas,''simone','andre', sep=' * ',end='#' )
+"""
+começamos a printar e falar de separado na hora de imprimir
+"""
