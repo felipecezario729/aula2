6b80cbdb086aa792b8358d667de3edb211fb78a9
Felipe Cezario <166965964+felipecezario729@users.noreply.github.com>
2025-07-11 21:14:56 -0300

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,7 @@
+# Tipos  int = > N ---- float => Real 
+print(10)
+print(type(10))
+print(10.9)
+print(type(10.9))
+print('seu nome')
+print(type('seu nome'))
---
+++ 6.py
@@ -0,0 +1,4 @@
+print(10 == 10) # verdadeiro
+print(10 == 11) # falso 
+print(10 != 11) # Verdadeiro 
+print(type(True))
