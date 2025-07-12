2545b7038f4853099c66381c94ff8352f4f1dc30
Felipe Cezario <166965964+felipecezario729@users.noreply.github.com>
2025-07-11 21:30:27 -0300

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,9 @@
+# variaveis são usadas para salvar 
+# algo na memória do computador 
+# uso nome_variavel = Expressão 
+nome = 'felipe'
+sobrenome = 'cezario'  
+print('nome : ',nome)
+print('sobrenome : ',sobrenome)
+concatenacao ='felipe' + 'cezario'
+print(concatenacao)
